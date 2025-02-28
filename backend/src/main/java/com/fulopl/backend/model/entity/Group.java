1c1ba71d1389fa04cd371a0c74c6aba192bb7573
Levente <145211298+fulopl@users.noreply.github.com>
2025-02-28 11:30:55 +0100

    add Group.java

---
+++ backend/src/main/java/com/fulopl/backend/model/entity/Group.java
@@ -0,0 +1,24 @@
+package com.fulopl.backend.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Set;
+
+@Entity
+public class Group {
+    @Id
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private AppUser owner;
+
+    @ManyToMany
+    private Set<AppUser> members;
+
+
+}
