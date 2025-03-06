7d20d9b75ccc8c9c43cc3346745cfc20bb3922c5
Levente <145211298+fulopl@users.noreply.github.com>
2025-03-06 10:26:17 +0100

    add getters

--- backend/src/main/java/com/fulopl/backend/model/entity/Role.java
+++ backend/src/main/java/com/fulopl/backend/model/entity/Role.java
@@ -11,6 +11,10 @@ public class Role {
     private Long id;
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
