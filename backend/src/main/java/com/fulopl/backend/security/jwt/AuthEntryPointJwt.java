748c80d2dd5dfdef72dcea6ae336a61dc1d9b288
Levente <145211298+fulopl@users.noreply.github.com>
2025-03-06 10:28:39 +0100

    add AuthEntryPointJwt.java

---
+++ backend/src/main/java/com/fulopl/backend/security/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,30 @@
+package com.fulopl.backend.security.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response
+            , AuthenticationException authException) throws IOException, ServletException {
+        String errorMsg = authException.getMessage();
+        logger.error("Security error: {}", errorMsg);
+        //response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write("{\"error\": \"" + errorMsg + "\"}");
+    }
+}
