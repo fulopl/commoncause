bb489a3e5af7e364dee3a66595c75622b7eee542
Levente <145211298+fulopl@users.noreply.github.com>
2025-03-19 15:19:20 +0100

    add registration page and user form

---
+++ frontend/src/components/UserForm.jsx
@@ -0,0 +1,48 @@
+import {useState} from "react";
+
+const UserForm = ({user, disabled, onSave}) => {
+
+    const [username, setUsername] = useState(user?.username ?? "");
+    const [password, setPassword] = useState(user?.password ?? "");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSave({username: username, password: password});
+    }
+
+    return <>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    autoComplete="off"
+                    placeholder="enter username here"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    name="username"
+                    id="username"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    autoComplete="off"
+                    placeholder="enter password here"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="password"
+                    id="password"
+                />
+            </div>
+            <div className="buttons">
+                <button type="submit" disabled={disabled}>
+                    {user.username ? "Update user data" : "Register"}
+                </button>
+            </div>
+        </form>
+    </>
+}
+
+export default UserForm;
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,5 +1,79 @@
+import UserForm from "../components/UserForm";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import Loading from "../components/Loading";
+
+const registerUser = (user) => {
+    return fetch("/api/user/register",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        }).then(res => res.text());
+}
+
 function Registration() {
-    return <>Registration</>
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [isRegistered, setRegistered] = useState(false);
+    const [isUsernameInUse, setUsernameInUse] = useState(false);
+
+    const handleRegister = (user) => {
+        setLoading(true);
+        registerUser(user).then(response => {
+                setLoading(false);
+                if (response === "Username not available.") setUsernameInUse(true)
+                else setRegistered(true);
+            }
+        );
+    }
+
+    if (isLoading) {
+        return <Loading/>;
+    }
+
+    if (isUsernameInUse) {
+        return (
+            <div className="container-main">
+                <div className="textbox-main">
+                    <h2>Username already in use.</h2>
+                    <h2>Please choose a different one!</h2>
+                    <button type="button" onClick={() => setUsernameInUse(false)}>
+                        OK
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    if (isRegistered) {
+        return (
+            <div className="container-main">
+                <div className="textbox-main">
+                    <h2>Account created.</h2>
+                    <h2>Please sign in!</h2>
+                    <button type="button" onClick={() => navigate("/sign-in")}>
+                        OK
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="container-main">
+            <div className="textbox-main">
+                <h2>Registration</h2>
+                <UserForm
+                    user={{username: ""}}
+                    disabled={isLoading}
+                    onSave={handleRegister}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Registration;
