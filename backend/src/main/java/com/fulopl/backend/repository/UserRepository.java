27dff443feac00f0c3fdd93dfb07a39bdd020e9b
Levente <145211298+fulopl@users.noreply.github.com>
2025-03-26 09:10:36 +0100

    add Repository annotation

--- backend/src/main/java/com/fulopl/backend/repository/UserRepository.java
+++ backend/src/main/java/com/fulopl/backend/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.fulopl.backend.repository;
 
 import com.fulopl.backend.model.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String username);
