497b61744f78e6e2eb82b6698b76a9604c4b36c4
Levente <145211298+fulopl@users.noreply.github.com>
2025-04-01 17:21:30 +0200

    add login conditions to layout

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,24 +1,34 @@
 import {Link, Outlet} from "react-router-dom";
+import {useUser} from "../context/UserProvider.jsx";
 
 function Layout() {
+    const {user} = useUser();
+
     return <>
         <nav>
             <ul>
                 <li>
                     <Link to="/">Main</Link>
                 </li>
-                <li>
-                    <Link to="/groups">Groups</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/logout">Logout</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
+                {user === null ?
+                    <>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                    </>
+                    :
+                    <>
+                        <li>
+                            <Link to="/groups">Groups</Link>
+                        </li>
+                        <li>
+                            <Link to="/logout">Logout</Link>
+                        </li>
+                    </>
+                }
             </ul>
         </nav>
         <Outlet/>
